684c11bf251658f0d3724bf479b5adfe9df164ef
Aftab Hussain <aftabh@node.llvm-ah.xcap.emulab.net>
2018-02-07 20:39:49 -0700

    refactor

---
+++ lcd-ex-1/TXT/gentxt.sh
@@ -0,0 +1,5 @@
+#!/bin/tcsh
+(cat -n ../my_device.c)>&my_device.c.txt
+(cat -n ../device.h)>&device.h.txt
+(cat -n ../device_manager.c)>&device_manager.c.txt
+
--- lcd-ex-1/gentxt.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/tcsh
-(cat -n my_device.c)>&my_device.c.txt
-(cat -n device.h)>&device.h.txt
-(cat -n device_manager.c)>&device_manager.c.txt
-
